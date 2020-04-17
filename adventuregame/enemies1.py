74d62f0910e8381910ae7a957d1528fc4fe4636e
Zachary Rohman <zachary.rohman@maine.edu>
2020-04-16 22:29:04 -0400

    #1 new enemies

--- adventuregame/enemies1.py
+++ adventuregame/enemies1.py
@@ -30,7 +30,7 @@ class Bats(Enemy):
 class Witch(Enemy):
     def __init__(self):
         super().__init__(name="Witch", hp=5, damage=15)
-        
+
 class Ogre(Enemy):
     def __init__(self):
         super().__init__(name="Ogre", hp=30, damage=15)
