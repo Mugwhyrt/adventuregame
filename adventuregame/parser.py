d654fc8ba9c7364846bcaf0bcda515b42941c0af
Zachary Rohman <zachary.rohman@maine.edu>
2020-05-13 11:27:34 -0400

    #2 implemented verb and preposition dictionaries, articles array

--- adventuregame/parser.py
+++ adventuregame/parser.py
@@ -13,9 +13,33 @@ __author__ = "Zachary Rohman"
 
 
 # Verb Dictionary
-verbs = {}
+verbs = {"go" : ["go", "head", "move", "walk", "run", "travel", "leave",
+                 "pass", "journey",  "depart", "advance", "exit"],
+         "attack" : ["attack", "kill", "assault", "beat", "hit", "hurt",
+                     "bash", "assail", "harm", "strike", "stab", "whop",
+                     "wallop", "whack", "bop"],
+         "use" : ["use", "equip", "employ", "wield", "manipulate",
+                  "utilize", "adorn", "furnish", "arm"],
+         "look" : ["look", "search", "inspect", "view", "peep",
+                   "scrutinize", "survey", "observe", "review",
+                   "probe", "investigate"],
+         "buy" : ["buy", "purchase", "procure", "acquire", "obtain",
+                  "take"],
+         "steal" : ["steal",  "loot", "snatch", "pickpocket", "pilfer",
+                    "pilfer", "swipe", "pinch", "poach", "shoplift"],
+         "flee" : ["flee", "escape","decamp", "scram", "hightail", "bolt",
+                   "disappear", "vamoose", "skedaddle", "scram"],
+         "eat" : ["eat", "drink", "consume", "devour", "swallow",
+                  "dine", "bite", "chew", "snack", "sup", "wolf",
+                  "feast", "gulp", "slup", "guzzle", "quaff", "gargle"]}
 # Preposition Dictionary
-prepositions = {}
+prepositions = {"with" : ["with", "using"],
+                "under" : ["under", "below", "beneath"],
+                "above" : ["above", "over"],
+                "in" : ["in", "among", "amongst", "within", "inside",
+                        "amidst", "around"]}
+# Articles Array
+articles = ["the", "a", "an", "ye", "thee"]
 
 # Parser Method
 # Receives user input (String) and lists of available actions and nouns
@@ -26,3 +50,5 @@ def parser():
     # which action should be taken
     wordTable = ["verb", "direct object", "preposition", "indirect object"]
     return wordTable
+
+
