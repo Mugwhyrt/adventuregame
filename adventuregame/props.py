d9aee1899d4b9158a9b6108cacbff26a5acd556a
Zachary Rohman <zachary.rohman@maine.edu>
2020-05-16 10:00:49 -0400

    #5 base implementation for Prop class

--- adventuregame/props.py
+++ adventuregame/props.py
@@ -1,5 +1,18 @@
 """
 Props
 
-A super class to define general behavior for any non-player object
+A super class to define general behavior for any object that interacts with
+the player
 """
+
+class Prop():
+    def __init__(self, nouns, actions, description, children):
+        self.nouns = nouns
+        self.actions = actions
+        self.description = description
+        self.children = children
+
+    def __str__(self):
+        return self.description
+
+    
